a15b4058e039861d4b9412b8037e658adcabecbe
OnionKiller <tomi@csilling.com>
2020-12-03 22:42:12 +0100

    created data inclusion

--- EnvTest/src/Data/init_data.h
+++ EnvTest/src/Data/init_data.h
@@ -1,2 +1,14 @@
 #pragma once
 
+#include "data.h"
+#include "raw_data.h"
+
+inline simple_failure_times init_failure_datas()
+{
+	auto return_ = simple_failure_times();
+	for (auto I : pdf04925834::list)
+	{
+		return_.add_failure(I);
+	}
+	return return_;
+};
