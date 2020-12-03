ee136b726a105031e2ae0d3e87d2844ff2aed2a9
OnionKiller <tomi@csilling.com>
2020-12-03 17:39:57 +0100

    solver implemented TODO parameter passing in a more organized way

--- EnvTest/src/Data/rejection_sampling/impl.cpp
+++ EnvTest/src/Data/rejection_sampling/impl.cpp
@@ -1,7 +1,76 @@
 #include "../modell_factory.h"
 #include "impl.h"
+#include <algorithm>
+#include <execution>
+#include <random>
+#include <functional>
+#include <ranges>
 
 void rejection_sampler::setData(simple_failure_times ftimes)
 {
 	this->failures_ = ftimes;
+}
+
+void rejection_sampler::setModell(imperfect_virtualage_likelihood modell)
+{
+	this->L_ = modell;
+}
+
+std::vector<double> rejection_sampler::solve(simulation_params param)
+{
+	std::default_random_engine gen;
+	auto U = std::bind(std::uniform_real_distribution(0., 1.),gen);
+	auto i = param.sample_size;
+	L_.set_data(failures_);
+	//sample
+	for (; i-- > 0;)
+	{
+		sample_result tmp_;
+		tmp_.accepted = false;
+		tmp_.L = L_.get_likelihood();
+		tmp_.params = L_.get_params();
+		raw_result_.push_back(tmp_);
+	}
+	//get g(x) as const
+	double L_max = std::max_element(std::execution::par,raw_result_.begin(), raw_result_.end(), [](const sample_result& A, const sample_result& B) {return A.L < B.L; })->L;
+	//rejection sampling
+	std::for_each(std::execution::par, raw_result_.begin(), raw_result_.end(), [&U, &L_max](sample_result& A) {
+		auto u = U();
+		if (u < A.L / L_max)
+			A.accepted = true;
+		else
+			A.accepted = false;
+		});
+	//get estimation from result
+
+	return std::vector<double>();
+}
+
+std::vector<sample_result> rejection_sampler::get_raw_result()
+{
+	return raw_result_;
+}
+
+std::vector<double> rejection_sampler::estimet_result_()
+{
+	if (raw_result_.empty())
+		throw std::exception("Internal error: raw_result is empty.");
+	auto example = *raw_result_.begin();
+	std::vector<double> avg(example.params.size(), 0);
+	decltype(raw_result_) filtered_result_{};
+//	for (sample_result I : raw_result_ | std::views::filter([](sample_result A) {return A.accepted; })) -- nice 2020 feature may use later
+//		filtered_result_.push_back(I);
+	for (auto I : raw_result_){
+		if (I.accepted)
+			filtered_result_.push_back(I);
+	}
+	auto accepted_number = filtered_result_.size();
+	avg = std::transform_reduce(std::execution::par, filtered_result_.begin(), filtered_result_.end(), avg,
+		[](const std::vector<double>& A, const std::vector<double>& B) {
+			auto C = std::vector<double>(A.size(),0);
+			std::transform(A.begin(), A.end(), B.begin(), C.begin(), std::plus<>());
+			return C;
+		}, [](sample_result& A) {return A.params; });
+	std::for_each(avg.begin(), avg.end(), [accepted_number](double& param) {param = param / double(accepted_number); });
+	return avg;
 }
--- EnvTest/src/Data/rejection_sampling/impl.h
+++ EnvTest/src/Data/rejection_sampling/impl.h
@@ -3,6 +3,18 @@
 
 #include "../modell_factory.h"
 #include "../data.h"
+#include <vector>
+
+struct simulation_params
+{
+	size_t sample_size;
+};
+
+struct sample_result {
+	bool accepted = false;
+	std::vector<double> params;
+	double L;
+};
 
 //interface to rejection sampler
 class rejection_sampler
@@ -10,8 +22,12 @@ class rejection_sampler
 public:
 	void setData(simple_failure_times ftimes);
 	void setModell(imperfect_virtualage_likelihood modell);
-	//solution solve(simulation_params);
+	std::vector<double> solve(simulation_params params); 
+	std::vector<sample_result> get_raw_result();
 protected:
 	simple_failure_times failures_;
+	imperfect_virtualage_likelihood L_;
 	//implement modell here
+	std::vector<sample_result> raw_result_;
+	std::vector<double> estimet_result_();
 };
