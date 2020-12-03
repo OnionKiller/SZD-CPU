df58ff066b5109da8f1d6128b4fbb431c45abaf0
OnionKiller <tomi@csilling.com>
2020-12-03 22:42:31 +0100

    type decision reimplemented

--- EnvTest/src/Modell/function_param.h
+++ EnvTest/src/Modell/function_param.h
@@ -3,34 +3,44 @@
 #include <type_traits>
 #include <random>
 #include <functional>
+#include <typeinfo>
 
 template<typename dataType>
 class function_param
 {
 public:
+	typedef std::conditional_t<
+		std::is_integral_v<dataType>,
+		std::uniform_int_distribution<dataType>,
+			std::conditional_t<
+			std::is_floating_point_v<dataType>,
+			std::uniform_real_distribution<dataType>,
+			void>
+		> distributionType;
 	function_param() = delete;
-	template <typename T = dataType, std::enable_if_t < std::is_integral < T >::value, bool > = true >
 	function_param(dataType min, dataType max) :min(min), max(max) {
-		std::uniform_int_distribution<dataType> distribution(min, max);
-		random_gen = [&, distribution]()->dataType {return distribution(this->generator); };
+		distribution = distributionType(min, max);
 	};
-	template <typename T = dataType, std::enable_if_t < std::is_floating_point < T >::value, bool > = true >
-	function_param(dataType min, dataType max) :min(min), max(max) {
-		std::uniform_real_distribution<dataType> distribution(min, max);
-		random_gen = [&, distribution]()->dataType {return distribution(this->generator); };
-	};
+	function_param(function_param& C)
+	{
+		min = C->min;
+		max = C->max;
+		distribution = distributionType(min, max);
+	}
 	//TODO add fallback if no type is accessible #SFINALEhell
 	//function_param(dataType min, dataType max) :min(min), max(max) {
 	//	static_assert(std::is_floating_point_v<dataType> || std::is_integral_v<dataType>, \
 	//		"Can't generate random value from non arithmetic values.");
 	//}
 	dataType get_random_variable() {
-		auto dVal = random_gen();
+		auto dVal = distribution(generator);
 		return dVal;
 	}
 private:
 	dataType min;
 	dataType max;
 	std::default_random_engine generator;
-	std::function<dataType()> random_gen;
+	distributionType distribution;
+	//FOR FUN ONLY NOT TESTED
+	auto binded_gen()-> decltype(distribution(generator)) { return distribution(generator); };
 };
