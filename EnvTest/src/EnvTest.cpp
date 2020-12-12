b93b555f1bf7155f9de51b10ba24eac534153936
OnionKiller <tomi@csilling.com>
2020-12-12 01:01:32 +0100

    test implemented

--- EnvTest/src/EnvTest.cpp
+++ EnvTest/src/EnvTest.cpp
@@ -18,11 +18,24 @@ int main()
     rejection_sampler<imperfect_virtualage_likelihood> solver;
     solver.setModell(modell);
     solver.setData(failures);
-    auto begin = std::chrono::steady_clock::now();
-    params.sample_size = 1000000;
-    auto result = solver.solve(params);
-    auto end = std::chrono::steady_clock::now();
-    auto est_time = std::chrono::duration_cast<std::chrono::seconds>(end - begin).count();
-    std::cout << "Done in " << est_time << " seconds !" << std::endl;
-    std::cout << "Estimeted params are: \nBeta : " << result[0] << "\nEta  : " << result[1] << "\nAr   : " << result[2] << "\nAp   : " << result[3] << std::endl;
+    std::cout << "raw_csv"<<std::endl;
+    std::cout << "run time in ms,sample size,beta,eta,ar,ap"<<std::endl;
+    for (auto ssize = 10; ssize <= 10000000; ssize *= 10) {
+        for (auto I = 10; I-- > 0;) {
+            auto begin = std::chrono::steady_clock::now();
+            params.sample_size = ssize;
+            auto result = solver.solve(params);
+            auto end = std::chrono::steady_clock::now();
+            auto est_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+            std::cout << est_time << "," << ssize << "," << result[0] << "," << result[1] << "," << result[2] << "," << result[3] << std::endl;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
