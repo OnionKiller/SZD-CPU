6e9cb50b29f8fe1e69330cfbeff018fadf6a9a86
OnionKiller <tomi@csilling.com>
2020-12-19 00:23:13 +0100

    write to file, measure parralel, and serial too

--- EnvTest/src/EnvTest.cpp
+++ EnvTest/src/EnvTest.cpp
@@ -5,6 +5,7 @@
 #include "Modell/rejection_sampling/full_impl.h"
 #include "Modell/modell_factory.h"
 #include "Data/init_data.h"
+#include <fstream>
 
 
 
@@ -18,19 +19,26 @@ int main()
     rejection_sampler<imperfect_virtualage_likelihood> solver;
     solver.setModell(modell);
     solver.setData(failures);
-    std::cout << "raw_csv"<<std::endl;
-    std::cout << "run time in ms,sample size,beta,eta,ar,ap"<<std::endl;
+    std::fstream file("timed_measurement_c_p_data.csv", std::fstream::trunc | std::fstream::out);
+    file << "raw_csv"<<std::endl;
+    file << "parralel,run time in ms,sample size,beta,eta,ar,ap"<<std::endl;
     for (auto ssize = 10; ssize <= 10000000; ssize *= 10) {
-        for (auto I = 5; I-- > 0;) {
+        for (auto I = 100; I-- > 0;) {
             auto begin = std::chrono::steady_clock::now();
             params.sample_size = ssize;
             auto result = solver.solve(params);
             auto end = std::chrono::steady_clock::now();
             auto est_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
-            std::cout << est_time << "," << ssize << "," << result[0] << "," << result[1] << "," << result[2] << "," << result[3] << std::endl;
+            file << false << "," << est_time << "," << ssize << "," << result[0] << "," << result[1] << "," << result[2] << "," << result[3] << std::endl;
+            begin = std::chrono::steady_clock::now();
+            params.sample_size = ssize;
+            result = solver.solve_p(params);
+            end = std::chrono::steady_clock::now();
+            est_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+            file << true << "," << est_time << "," << ssize << "," << result[0] << "," << result[1] << "," << result[2] << "," << result[3] << std::endl;
         }
     }
-    
+    file.close();
     
     
     
